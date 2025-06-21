9dd31d375889125cd65eb9920b86caee8b3d385f
V-Silpin <varun.mohanta323@gmail.com>
2025-06-21 08:17:50 +0530

    Modified params

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.include_router(router)
 def main():
     print("Hello from crud-api-server-python!")
     # Optionally, you can run the server here for local development
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
